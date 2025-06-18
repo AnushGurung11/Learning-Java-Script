ca81dce3b2a96578459867e7f55aac8a951c13a6
AnushGurung11 <anushgrg15@gmai.com>
2025-06-18 21:51:10 +0545

    Basic variables

--- mainJS.js
+++ mainJS.js
@@ -1,1 +1,10 @@
-console.log("Hello World"); 
+//Variables in JS 
+
+//using let for varibales 
+let name = "ram"; 
+
+//using const for constants
+const PIE = 3.14; 
+
+console.log(name); 
+console.log(PIE); 

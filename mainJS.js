52b36e97c402125e2b00eaf21fc262f59b993c35
AnushGurung11 <anushgrg15@gmai.com>
2025-06-19 13:54:12 +0545

    creating objects on JS

--- mainJS.js
+++ mainJS.js
@@ -1,7 +1,20 @@
-//Creating a functio for adding 2 numbers 
+//Creating Objects in JS
 
-function sum(p1,p2){
-    return p1+p2; 
-}
+const car = new Object(); 
 
-console.log(sum(2,3)); 
+car.name = "Tesla"; 
+car.model= "S";
+
+console.log(car.name); 
+console.log(car.model); 
+
+// Another way of creating objects in JS
+
+const car2 = {
+    name: "Nishan",
+    model : "M22"
+    
+}; 
+
+console.log(car2.name); 
+console.log(car2.model);
